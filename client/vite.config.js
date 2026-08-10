08b05bbd5a327791b024b8ab00839070c97c2f16
ubong <ubongnt@gmail.com>
2026-08-10 04:54:19 +0100

    chore: remove all tests and test dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-    css: true,
-  },
 })
